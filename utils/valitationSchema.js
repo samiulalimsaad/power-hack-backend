79d8904c5bd4c4a25b49e13ceb1a8c7985c2cd83
samiulalimsaad <saadnbiuraj@gmail.com>
2022-07-01 10:49:38 +0600

    validation schema added

---
+++ utils/valitationSchema.js
@@ -0,0 +1,26 @@
+import * as Yup from "yup";
+
+export const LoginValidationSchema = Yup.object({
+    name: Yup.string().required("Name is required").trim(),
+    password: Yup.string().required("Password is required").min(8),
+});
+
+export const SignUpValidationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    phone: Yup.string().required("Phone Number is required"),
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Email is required"),
+    password: Yup.string()
+        .min(8, "Password must contain at least 8 characters")
+        .required("Enter your password required"),
+});
+
+export const BillingFormValidationSchema = Yup.object({
+    fullName: Yup.string().required("Name is required"),
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Email is required"),
+    phone: Yup.string().required("Phone Number is required"),
+    paidAmount: Yup.string().required("amount is required"),
+});
