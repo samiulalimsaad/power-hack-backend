67b0c90c00575abc769106bbc2ecbe0107bf67c2
samiulalimsaad <saadnbiuraj@gmail.com>
2022-07-01 12:10:46 +0600

    schema updated

--- Models/User.Model.js
+++ Models/User.Model.js
@@ -1,10 +1,10 @@
-import { model, Schema } from "mongoose";
-
+import mongoose from "mongoose";
+const { model, Schema } = mongoose;
 // Create a Schema corresponding to the document interface.
 const userSchema = new Schema(
     {
         fullName: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         phone: { type: String, required: true },
         password: { type: String, required: true },
     },
--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -1,19 +1,22 @@
 import * as Yup from "yup";
 
 export const LoginValidationSchema = Yup.object({
-    name: Yup.string().required("Name is required").trim(),
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Name is required")
+        .trim(),
     password: Yup.string().required("Password is required").min(8),
 });
 
 export const SignUpValidationSchema = Yup.object({
-    name: Yup.string().required("Name is required"),
+    fullName: Yup.string().required("Name is required"),
     phone: Yup.string().required("Phone Number is required"),
     email: Yup.string()
         .email("Enter a valid email")
         .required("Email is required"),
     password: Yup.string()
         .min(8, "Password must contain at least 8 characters")
-        .required("Enter your password required"),
+        .required("Password required"),
 });
 
 export const BillValidationSchema = Yup.object({
